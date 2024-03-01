e993a298612d3cf36a9d495df2e0c9806d95ec6c
Hamdoune001 <148764285+Hamdoune001@users.noreply.github.com>
2024-03-01 09:48:20 +0100

    Tab Done graphique Done Performance Done Accessibility Done                   -The End-

--- Js/home.js
+++ Js/home.js
@@ -7,3 +7,57 @@ document.addEventListener("DOMContentLoaded", function () {
         article.classList.add("enjolive-cadre");
     });
 });
+//Tab_inter
+document.addEventListener("DOMContentLoaded", function () {
+    var rows = document.querySelectorAll("tbody tr");
+
+    rows.forEach(function (row) {
+        row.addEventListener("mouseover", function () {
+            this.style.backgroundColor = "rgba(192,75,75,0.7)";
+        });
+
+        row.addEventListener("mouseout", function () {
+            this.style.backgroundColor = "";
+        });
+    });
+});
+
+//Graphique
+var ctx = document.getElementById("probabilityChart").getContext("2d");
+var probabilityChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Kylian Mbappé au Real Madrid'],
+        datasets: [{
+            label: 'Probabilité',
+            data: [90], // Changez cette valeur en fonction de la probabilité
+            backgroundColor: 'rgba(192,75,75,0.7)',
+            borderColor: 'rgb(255,0,0)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                max: 100
+            }
+        }
+    }
+});
+// Fonction pour mettre en bleu les @ suivies de lettres
+function colorizeAtSigns() {
+    var paragraphs = document.querySelectorAll('.comments-section p');
+
+    paragraphs.forEach(function (paragraph) {
+        var content = paragraph.innerHTML;
+        var regex = /@([a-zA-Z]+)/g;
+        var replacedContent = content.replace(regex, '<span class="blue-at">@$1</span>');
+        paragraph.innerHTML = replacedContent;
+    });
+}
+
+// Appeler la fonction lors du chargement de la page
+window.onload = function () {
+    colorizeAtSigns();
+};
